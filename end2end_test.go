ba249cde9f42af767b0ab69919e6c19259276f94
Alexander Trakhimenok <alex@trakhimenok.com>
2025-08-08 21:40:06 +0100

    fix: TestEndToEnd to use sub test

--- end2end_test.go
+++ end2end_test.go
@@ -3,11 +3,12 @@ package end2end
 import "testing"
 
 func TestEndToEnd(t *testing.T) {
-
-	defer func() {
-		if err := recover(); err == nil {
-			t.Fatal("should panic on nil parameters")
-		}
-	}()
-	TestDalgoDB(nil, nil, nil, true)
+	t.Run("panics_on_nil_params", func(t *testing.T) {
+		defer func() {
+			if err := recover(); err == nil {
+				t.Fatal("should panic on nil parameters")
+			}
+		}()
+		TestDalgoDB(nil, nil, nil, true)
+	})
 }
